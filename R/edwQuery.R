f74cd7e009e50b8dabaf5aa0dc29fa78e99c99fd
unknown <p348411@Z-PIPC025.wa.providence.org>
2014-05-04 10:47:17 -0700

    added order parameter

--- R/edwQuery.R
+++ R/edwQuery.R
@@ -5,7 +5,8 @@
 #' @param table_name - name of the desired table
 #' @param max - used to return the TOP n records - use a large number for all records
 #' @param fields - single string of field names separated by commas
-#' @param where - additional where clauses
+#' @param where - additional WHERE clauses
+#' @param order - additional ORDER BY clauses
 #' @param resource - Alias for the database connection
 #' @keywords sql
 #’ @export
@@ -13,12 +14,13 @@
 #' sql_query("")
 
 edwQuery <- function(schema, 
-                      table_name, 
-                      max=10, 
-                      fields="*", 
-                      where="1=1", 
-                      resource="Phloston"
-                      ) {
+                     table_name, 
+                     max=10, 
+                     fields="*", 
+                     where="1=1",
+                     order = NA,
+                     resource="Phloston"
+                     ) {
   
   require(RODBC)
   
@@ -36,7 +38,12 @@ edwQuery <- function(schema,
   sql.where <- sprintf("AND %s ", 
                        where)
   
-  sql <- paste0(sql.select, sql.from, sql.where)
+  if(!is.na(order)) {
+    sql.order <- sprintf("ORDER BY %s ", 
+                       order)
+  }
+
+  sql <- paste0(sql.select, sql.from, sql.where, sql.order)
   
   #result <- sqlTables(conn)
   queryResult <- sqlQuery(conn, 
