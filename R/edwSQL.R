9a0140bacc41b987f0ff55a961fb5bb9c555cb79
Rollie Parrish <rparrish@flightweb.com>
2014-11-15 14:35:56 -0800

    added new function

---
+++ R/edwSQL.R
@@ -0,0 +1,48 @@
+#' edwSQL
+#'
+#' passes a SQL query to the EDW. Commented lines in the .sql file must be preceded by '--'. Blocked comments are not supported.
+#'
+#' @param sql - sql query or the path & name of a .sql file
+#' @param resource - Alias for the database connection
+#' @param file switch indicating if sql parameter is a .sql file or a command. Default is TRUE (requires .sql file)
+#' @return
+#' \item{data}{Query results as a data frame}
+#' \item{fields}{field names}
+#' \item{elapsed_seconds}{elapsed system time to run the query}
+#' \item{status_message}{the number of records & variables in the results}
+#'
+#' @keywords sql
+#' @export
+#'
+
+
+edwSQL <- function (sql="SELECT TOP 10 * FROM ProvidenceEpic.Finance.HospitalAccountBASE", resource = "Phloston", file=TRUE,...) {
+    start_time <- Sys.time()
+    conn <- odbcDriverConnect(connection_string(resource))
+
+    if (file) {
+        sql <- readLines(sql, ok = TRUE, warn = FALSE)
+    }
+    sql <- gsub("--.*", "", sql)
+    sql <- paste(sql, collapse = " ")
+
+    queryResult <- sqlQuery(conn, sql, stringsAsFactors = FALSE)
+    odbcClose(conn)
+    elapsed_seconds <- as.numeric(difftime(Sys.time(), start_time,
+                                           units = "secs"))
+    status_message <- paste0(format(nrow(queryResult), big.mark = ",",
+                                    scientific = FALSE, trim = TRUE),
+                             " records and ",
+                             format(length(queryResult),
+                                    big.mark = ",",
+                                    scientific = FALSE,
+                                    trim = TRUE),
+                             " columns were read from ",
+                             resource,
+                             " in ",
+                             round(elapsed_seconds, 2),
+                             " seconds.")
+    return(list(data = queryResult, fields = names(queryResult),
+                elapsed_seconds = elapsed_seconds,
+                status_message = status_message))
+}
