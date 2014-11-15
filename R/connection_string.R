5a44d0a3b4a44b9278f619e58b2ec2ccb941972d
Rollie Parrish <rparrish@flightweb.com>
2014-11-15 15:00:31 -0800

    Removed Aurora. Added Apollo & ClinicalAnalyitics

--- R/connection_string.R
+++ R/connection_string.R
@@ -10,11 +10,10 @@ connection_string <- function (resource, database=NULL, uid, pwd) {
   result <- switch(resource,
                    Trantor = "Driver=SQL Server;Server=EDWDBProd; Database=ProvidenceEpic",
                    Phloston = "Driver=SQL Server;Server=EDWDBDev; Database=ProvidenceEpic",
-                   Aurora = "Driver=SQL Server;Server=EDWDBDev; Database=CatalystStaging",
-                   Eva = sprintf("Driver=SQL Server;Server=wn1444; Database=%s; uid=%s; pwd=%s",
-                                 database,
-                                 uid,
-                                 pwd)
+                   ClinicalAnalytics = "Driver=SQL Server;Server=wn2591\\PremierPRD; Database=ClinicalAnalytics",
+                   Apollo = "Driver=SQL Server;Server=wn1444; Database=%s; uid=%s; pwd=%s",
+                   Eva = sprintf("Driver=SQL Server;Server=wn1444; Database=%s; uid=%s; pwd=%s")
                    )
   return(result)
 }
+
