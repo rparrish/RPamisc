02a2fbcd8423154b9363d8c1143cc8baea904670
Rollie Parrish <rparrish@flightweb.com>
2014-11-28 15:09:28 -0800

    upldated documentation

--- R/bg_window.R
+++ R/bg_window.R
@@ -2,16 +2,34 @@
 #'
 #' Get the BG data from the 18-24 hour post-op window,
 #'
-#' returns the window start & end time, min & max BG values,
-#' the number of values and the number over 180 for a
-#' CommunityPatientID with a given surgery date and surgery end time.
+#' returns the start & end time of the BG window, the minimum & maximum
+#' BG values, the total number of values and the number over 180.
 #'
 #' @param cp_id CommunityPatientID
 #' @param surgery_date The date of surgery in ISO format
 #' @param surgery_end_time  The surgery end time in HH:MM
-#' @return Data frame with cp_id  cp_id, start,end (of the BG Window) min, max, count_values, count_180
+#' @return Returns a data frame with cp_id, start,end (of the BG Window) min, max, count_values,
+#' & count_180.
 #'
-#' @keywords sql
+#' @keywords bg
+#' @examples
+#' ## single
+#' cp_id <- c("1501668269")
+#' surgery_date <- "2014-10-02"
+#' surgery_end_time <- "13:22"
+#'
+#' bg_window(cp_id, surgery_date, surgery_end_time)
+#'
+#' ## multiples
+#' cases <- data.frame(cp_id = c("1504685006","1503010730", "1502850183"),
+#'                     surgery_date = c("2014-09-25", "2014-10-01", "2014-09-30"),
+#'                     surgery_end_time = c("16:58", "16:49", "13:40"))
+#'
+#' bg_window_data <- apply(cases[, c('cp_id', 'surgery_date', 'surgery_end_time')], 1,
+#'                      function(y) bg_window(y['cp_id'], y['surgery_date'], y['surgery_end_time']))
+#'
+#' do.call(rbind, bg_window_data)
+#'
 #' @export
 #'
 
