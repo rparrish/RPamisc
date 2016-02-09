bb42f5f1ab1c6efa33950e002061462597e40656
rparrish <rparrish@flightweb.com>
2016-02-09 07:17:27 -0800

    updated Apollo server name

--- R/connection_string.R
+++ R/connection_string.R
@@ -19,7 +19,7 @@ connection_string <- function (resource, database=NULL, uid=getOption("Apollo_ui
                    Trantor = "Driver=SQL Server;Server=EDWDBProd; Database=ProvidenceEpic",
                    Phloston = "Driver=SQL Server;Server=EDWDBDev; Database=ProvidenceEpic",
                    ClinicalAnalytics = "Driver=SQL Server;Server=wn2591\\PremierPRD; Database=ClinicalAnalytics",
-                   Apollo = paste0("Driver=SQL Server;Server=wn1444; Database=Apollo_Spokane; uid=", uid, "; pwd=", pwd),
+                   Apollo = paste0("Driver=SQL Server;Server=wn57066; Database=Apollo_Spokane; uid=", uid, "; pwd=", pwd),
                    Eva = paste0("Driver=SQL Server;Server=wn1444; Database=Testing; uid=", uid, "; pwd=", pwd)
                    )
   return(result)
