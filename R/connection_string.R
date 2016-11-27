0112cd2b95fe131c2a9ef4354428c37cc3f578fa
unknown <p348411@EWA892277.wa.providence.org>
2016-11-27 15:12:17 -0800

    added connecions. closes #20

--- R/connection_string.R
+++ R/connection_string.R
@@ -9,6 +9,9 @@
 #'   \item{Trantor}{EDWDBProd}
 #'   \item{Phloston}{EDWDBDev}
 #'   \item{PHC_CA}{PHC_CA}
+#'   \item{PHC_Shared}{PHC_Shared}
+#'   \item{PHC_Training}{PHC_Training}
+#'   \item{eNICQ}{eNICQ}
 #'   \item{ClinicalAnalytics}{\\\\wn2591\\PremierPRD}
 #'   \item{Apollo}{\\\\wn1444 Apollo_Spokane}
 #' }
@@ -31,6 +34,9 @@ connection_string <- function (resource="custom", custom, uid=getOption("Apollo_
                    Trantor = "Driver=SQL Server;Server=EDWDBProd; Database=ProvidenceEpic",
                    Phloston = "Driver=SQL Server;Server=EDWDBDev; Database=ProvidenceEpic",
                    PHC_CA = "Driver=SQL Server;Server=wn23162;",
+                   PHC_Shared = "Driver=SQL Server;Server=wn23162; Database=PHC_Shared",
+                   PHC_Training = "Driver=SQL Server;Server=wn23162; Database=PHC_Training",
+                   eNICQ = "Driver=SQL Server;Server=wnc1479av\\prodsql2k8a; Database=eNICQ",
                    ClinicalAnalytics = "Driver=SQL Server;Server=wn2591\\PremierPRD; Database=ClinicalAnalytics",
                    Apollo = paste0("Driver=SQL Server;Server=wn57066; Database=Apollo_Spokane; uid=",
                                    uid = getOption("Apollo_uid"), "; pwd=", pwd = getOption("Apollo_pwd"))
